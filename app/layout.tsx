1495af514f7029c88af25175c2cc84cbe5c757b6
Luis Amorim <luisfelipeamorim@hotmail.com>
2026-03-30 11:21:38 -0300

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { ClerkProvider } from "@clerk/nextjs"
+import { Analytics } from "@vercel/analytics/next"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -36,6 +37,7 @@ export default function RootLayout({
         ) : (
           content
         )}
+        <Analytics />
       </body>
     </html>
   )
